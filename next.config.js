ec26405a4ca25858df33ce210b5cae2964c6113b
mpvreddy <130564594+mpvreddy@users.noreply.github.com>
2023-07-28 17:42:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,39 @@
 const nextConfig = {}
 
 module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfigsoryyy
